1e99a43ad3420444afb436dadaeccb2f65423ecc
LHOK-Business <lhok.business@gmail.com>
2026-09-12 14:16:36 -0400

    Hunain - created cta button with calendly link

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+// src/lib/constants.js
+
+// Update this once and both the header button and the quiz results
+// "Book a Call" button will use the same link.
+export const BOOKING_URL = "www.calendly.com/ima-meeting"; // TODO: replace with your real Calendly link
